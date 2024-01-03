9e6b8d2e7cbdbfff5ff2b149b4edeb6bad51bfb1
msvs_prasad <maddipatlasvsprasad@gmail.com>
2024-01-03 17:18:08 -0500

    feat: FIR AVAR analysis: RW input

---
+++ Functions/script_AVAR_analyzeRWinFIRfilter.m
@@ -0,0 +1,222 @@
+%%%%%%%%%%%%%%%%%%%% script_AVAR_analyzeRWinFIRfilter.m %%%%%%%%%%%%%%%%%%%
+%% Purpose:
+%   The purpose of this script is to analyze FIR filter with Random Walk
+%   input corrupted by White Noise using Allan Variance and MSE
+%
+% Author:  Satya Prasad
+% Created: 2024/01/02
+
+%% Prepare the workspace
+clear all %#ok<CLALL>
+close all
+clc
+
+%% Initialization
+rng('default')
+
+%% Define inputs and other parameters
+fir_noise_model      = 1;
+number_of_iterations = 500;
+
+list_of_fir_filter_orders   = [(1:4), (6:2:16), (24:8:48), 50, (56:8:96), (100:32:1000)]';
+number_of_fir_filter_orders = numel(list_of_fir_filter_orders);
+list_of_normalized_cutoff_frequencies   = [(0.02:0.02:0.1), (0.2:0.2:0.8)];
+number_of_normalized_cutoff_frequencies = numel(list_of_normalized_cutoff_frequencies);
+
+sampling_frequency   = 50; % [Hz]
+sampling_interval    = 1/sampling_frequency; % [second]
+number_of_time_steps = 2^19;
+
+p = floor(log2(number_of_time_steps));
+list_of_correlation_intervals   = 2.^(0:p-3)'; % List of correlation intervals
+list_of_correlation_time        = list_of_correlation_intervals*sampling_interval;
+number_of_correlation_intervals = numel(list_of_correlation_intervals);
+target_points = [round(number_of_time_steps/3), round(2*number_of_time_steps/3), ...
+                 number_of_time_steps];
+number_of_target_points = numel(target_points);
+
+% Noise parameters
+power_spectral_density  = 0.0004; % [unit^2 s]
+random_walk_coefficient = 0.02; % [unit/sqrt(s)]
+
+%% Estimate MSE and AVAR of error in FIR filter
+% Initialize variables
+estimated_MSE   = zeros(number_of_time_steps,number_of_fir_filter_orders,number_of_normalized_cutoff_frequencies);
+estimated_AVAR  = NaN(number_of_correlation_intervals,number_of_fir_filter_orders,number_of_normalized_cutoff_frequencies);
+calculated_AVAR = NaN(number_of_correlation_intervals,number_of_fir_filter_orders,number_of_normalized_cutoff_frequencies);
+
+max_fir_filter_order = max(list_of_fir_filter_orders);
+time_vector = sampling_interval*(0:number_of_time_steps-1)';
+for index_iter = 1:number_of_iterations
+%% Synthesize the input signal
+white_noise  = fcn_AVAR_generateWhiteNoise(power_spectral_density,...
+               sampling_frequency,number_of_time_steps+max_fir_filter_order); % White noise
+random_walk  = fcn_AVAR_generateRandomWalk(random_walk_coefficient,...
+               sampling_frequency,number_of_time_steps+max_fir_filter_order); % Random walk
+random_walk  = random_walk - random_walk(max_fir_filter_order+1);
+input_signal = random_walk + white_noise;
+
+for i = 1:number_of_fir_filter_orders
+    fir_filter_order = list_of_fir_filter_orders(i);
+    
+    for j = 1:number_of_normalized_cutoff_frequencies
+        normalized_cutoff_frequency = list_of_normalized_cutoff_frequencies(j);
+        % Design FIR filter and Filter the input signal
+        fir_filter_num  = fir1(fir_filter_order,normalized_cutoff_frequency);
+        filtered_output = filter(fir_filter_num,1,...
+                                 input_signal(end-number_of_time_steps-fir_filter_order+1:end));
+        filtered_output = filtered_output(fir_filter_order+1:end);
+        actual_error    = random_walk(end-number_of_time_steps+1:end)-filtered_output;
+        
+        % MSE with Random Walk as true input
+        estimated_MSE(:,i,j) = estimated_MSE(:,i,j) + (actual_error.^2);
+        if index_iter==1
+            % AVAR of error with Random Walk as true input
+            estimated_AVAR(:,i,j) = fcn_AVAR_favar([actual_error; 0],list_of_correlation_intervals);
+            calculated_AVAR(:,i,j) = ...
+                fcn_AVAR_avarFIR(power_spectral_density,random_walk_coefficient,...
+                list_of_correlation_intervals,fir_filter_order,fir_filter_num,...
+                sampling_interval,fir_noise_model);
+        end % NOTE: END IF statement 'index_iter==1'
+    end % NOTE: END FOR loop 'number_of_normalized_cutoff_frequencies'
+end % NOTE: END FOR loop 'number_of_fir_filter_orders'
+end % NOTE: END FOR loop 'number_of_iterations'
+estimated_MSE = estimated_MSE/number_of_iterations;
+
+%% MSE and AVAR of error for no filter case
+NF_MSE_IN1  = (power_spectral_density/sampling_interval)*ones(number_of_time_steps,1);
+NF_AVAR_IN1 = fcn_AVAR_avarWhiteNoise(power_spectral_density,list_of_correlation_time);
+
+%% Plot the results
+default_color_map = jet(256);
+jf = java.text.DecimalFormat;   % To add comma to numbers
+%%% To show MSE is independent of signal length
+figure(01)
+clf
+width = 1555.6-40; height = 400; right = 20; bottom = 100;
+set(gcf, 'position', [right, bottom, width, height])
+custom_color_map = default_color_map(1:floor(256/number_of_normalized_cutoff_frequencies):256,:);
+for index_target = 1:number_of_target_points
+    legend_cell   = cell(number_of_normalized_cutoff_frequencies+1,1);
+    axis_position = [(85+(index_target-1)*475)/width, 0.1567, 415.6/width, 0.7683];
+    subplot(1,3,index_target)
+    hold on
+    grid on
+    yline(NF_MSE_IN1(target_points(index_target)),'m--','Linewidth',1.2)
+    legend_cell{1} = 'No Filter';
+    for i = 1:number_of_normalized_cutoff_frequencies
+        temp_var = estimated_MSE(target_points(index_target),:,i);
+        plot(list_of_fir_filter_orders,temp_var(:),'Color',custom_color_map(i,:),'Linewidth',1.2)
+        legend_cell{i+1} = ['$\omega_{n} =$ ' num2str(list_of_normalized_cutoff_frequencies(i))];
+    end % NOTE: END FOR loop 'number_of_normalized_cutoff_frequencies'
+    set(gca,'Position',axis_position,'xtick',[1e0 1e1 1e2 1e3],'ytick',[0 0.01 0.02 0.03],'XScale','log','FontSize',13)
+    if index_target==1
+        legend(legend_cell,'NumColumns',3,'Location','best','Interpreter','latex','FontSize',13)
+        ylabel('Mean Squared Error $[Unit^2]$','Interpreter','latex','FontSize',18)
+    end % NOTE: END if statement 'index_target==1'
+    xlabel('FIR Filter Order','Interpreter','latex','FontSize',18)
+    title(['$k = $ ' char(jf.format(target_points(index_target)-1))],'Interpreter','latex','FontSize',18)
+    ylim([0 0.03])
+end % NOTE: END for loop 'number_of_target_points'
+
+%%% MSE vs Area of AVAR
+custom_color_map = default_color_map(1:floor(256/number_of_normalized_cutoff_frequencies):256,:);
+legend_cell      = cell(number_of_normalized_cutoff_frequencies,1);
+figure(02)
+clf
+width = 540; height = 770.04; right = 100; bottom = 10;
+set(gcf, 'position', [right, bottom, width, height])
+axis_position = [0.1354, 432.72/height, 0.7696, 307.32/height];
+subplot(2,1,1)
+hold on
+grid on
+yline(NF_MSE_IN1(target_points(end)),'m--','Linewidth',1.2)
+for i = 1:number_of_normalized_cutoff_frequencies
+    temp_var = estimated_MSE(target_points(end),:,i);
+    plot(list_of_fir_filter_orders,temp_var(:),'Color',custom_color_map(i,:),'Linewidth',1.2)
+end % NOTE: END FOR loop 'number_of_normalized_cutoff_frequencies'
+set(gca,'Position',axis_position,'xtick',[1e0 1e1 1e2 1e3],'ytick',[0 0.01 0.02 0.03],'XScale','log','FontSize',13)
+ylabel('Mean Squared Error $[Unit^2]$','Interpreter','latex','FontSize',18)
+title('$(a)$','Interpreter','latex','FontSize',18)
+ylim([0 0.03])
+
+axis_position = [0.1354, 62.7/height, 0.7696, 307.32/height];
+subplot(2,1,2)
+hold on
+grid on
+yline(0.5*sum(NF_AVAR_IN1(1:end-1)+NF_AVAR_IN1(2:end)),'m--','Linewidth',1.2)
+legend_cell{1} = 'No Filter';
+for i = 1:number_of_normalized_cutoff_frequencies
+    plot(list_of_fir_filter_orders,...
+         0.5*sum(estimated_AVAR(1:end-1,:,i)+estimated_AVAR(2:end,:,i),1),...
+         'Color',custom_color_map(i,:),'Linewidth',1.2)
+    legend_cell{i+1} = ['$\omega_{n} =$ ' num2str(list_of_normalized_cutoff_frequencies(i))];
+end % NOTE: END FOR loop 'number_of_normalized_cutoff_frequencies'
+legend(legend_cell,'NumColumns',3,'Location','best','Interpreter','latex','FontSize',13)
+set(gca,'Position',axis_position,'xtick',[1e0 1e1 1e2 1e3],'ytick',[0 0.01 0.02 0.03 0.04],'XScale','log','FontSize',13)
+ylabel('Area of AVAR $[Unit^2]$','Interpreter','latex','FontSize',18)
+xlabel('FIR Filter Order','Interpreter','latex','FontSize',18)
+title('$(b)$','Interpreter','latex','FontSize',18)
+ylim([0 0.045])
+
+%%% Area of AVAR + Time domain demo
+index_freq = 1;
+figure(03)
+clf
+width = 1056.2+25; height = 400; right = 100; bottom = 100;
+set(gcf, 'position', [right, bottom, width, height])
+axis_position = [85/width, 0.1567, 415.6/width, 0.7683];
+subplot(1,2,1)
+hold on
+grid on
+plot(list_of_fir_filter_orders,...
+     0.5*sum(calculated_AVAR(1:end-1,:,index_freq)+calculated_AVAR(2:end,:,1),index_freq),...
+     'k','Linewidth',1.2)
+xline(3,'c--','Linewidth',1.2)
+xline(80,'b','Linewidth',1.2)
+xline(550,'m-.','Linewidth',1.2)
+legend(['$\omega_{n} =$ ' num2str(list_of_normalized_cutoff_frequencies(index_freq))],...
+       '$p=3$','$p=80$','$p=550$',...
+       'NumColumns',2,'Location','best','Interpreter','latex','FontSize',13)
+set(gca,'Position',axis_position,'xtick',[1e0 1e1 1e2 1e3],'XScale','log','FontSize',13)
+ylabel('Area of AVAR $[Unit^2]$','Interpreter','latex','FontSize',18)
+xlabel('FIR Filter Order','Interpreter','latex','FontSize',18)
+title('$(a)$','Interpreter','latex','FontSize',18)
+
+normalized_cutoff_frequency = list_of_normalized_cutoff_frequencies(index_freq);
+axis_position = [(85+80+415.584)/width, 0.1567, 415.6/width, 0.7683];
+subplot(1,2,2)
+hold on
+grid on
+plot(-1,-1,'Color',[0.7 0.7 0.7],'Linewidth',4)
+plot(-1,-1,'c.','Markersize',10)
+plot(-1,-1,'b','Linewidth',1.2)
+plot(-1,-1,'m','Linewidth',1.2)
+
+% Design FIR filter and Filter the input signal
+fir_filter_num  = fir1(3,normalized_cutoff_frequency);
+filtered_output = filter(fir_filter_num,1,input_signal);
+filtered_output = filtered_output(end-number_of_time_steps+1:end);
+plot(time_vector,filtered_output,'c.','Markersize',4)
+
+plot(time_vector,random_walk(end-number_of_time_steps+1:end),'Color',[0.7 0.7 0.7],'Linewidth',3)
+
+% Design FIR filter and Filter the input signal
+fir_filter_num  = fir1(80,normalized_cutoff_frequency);
+filtered_output = filter(fir_filter_num,1,input_signal);
+filtered_output = filtered_output(end-number_of_time_steps+1:end);
+plot(time_vector,filtered_output,'b','Markersize',1.2)
+
+% Design FIR filter and Filter the input signal
+fir_filter_num  = fir1(550,normalized_cutoff_frequency);
+filtered_output = filter(fir_filter_num,1,input_signal);
+filtered_output = filtered_output(end-number_of_time_steps+1:end);
+plot(time_vector,filtered_output,'m','Linewidth',1.2)
+
+legend('Reference Input','$p=3$','$p=80$','$p=550$','NumColumns',2,...
+       'Interpreter','latex','FontSize',13)
+set(gca,'Position',axis_position,'FontSize',13)
+ylabel('Amplitude $[Unit]$','Interpreter','latex','FontSize',18)
+xlabel('Time $[s]$','Interpreter','latex','FontSize',18)
+title('$(b)$','Interpreter','latex','FontSize',18)
+xlim([0 100])
---
+++ Functions/script_AVAR_demonstrateAVAROfFIR.m
@@ -0,0 +1,171 @@
+%%%%%%%%%%%%%%%%%%% script_AVAR_demonstrateAVAROfFIR.m %%%%%%%%%%%%%%%%%%%
+%% Purpose:
+%   The purpose of this script is to demonstrate FIR filter and AVAR of
+%   I/O of FIR filter.
+%
+% This script was written on 2023_09_19 by Satya Prasad
+% Questions or comments? szm888@psu.edu
+
+%% Prepare the workspace
+clear all %#ok<CLALL>
+close all
+clc
+
+%% Initialization
+rng('default')
+
+%% Define inputs and other parameters
+number_of_iterations = 100;
+% fir_noise_model -> 0: AVAR of output of FIR filter
+% fir_noise_model -> 1: Random walk corrupted by white noise
+% fir_noise_model -> 2: Constant corrupted by random walk and white noise
+fir_noise_model = 0;
+
+% Design an FIR filter
+fir_filter_order            = 50;
+normalized_cutoff_frequency = 0.25;
+fir_filter_num              = fir1(fir_filter_order,normalized_cutoff_frequency);
+
+sampling_frequency   = 50; % [Hz]
+sampling_interval    = 1/sampling_frequency; % [second]
+number_of_time_steps = 2^19;
+
+p = floor(log2(number_of_time_steps));
+list_of_correlation_intervals = 2.^(0:p-3)'; % List of correlation intervals
+
+% Noise parameters
+power_spectral_density  = 0.0004; % [unit^2 s]
+random_walk_coefficient = 0.02; % [unit/sqrt(s)]
+confidence_coefficient  = 0.95;
+noise_type              = 'rw';
+
+%%% Loop over 'number_of_iterations'
+matrix_fir_avar = NaN(p-2,number_of_iterations);
+for index_iter = 1:number_of_iterations
+    %% Synthesize the input signal
+    white_noise  = fcn_AVAR_generateWhiteNoise(power_spectral_density,...
+                   sampling_frequency,number_of_time_steps+fir_filter_order); % White noise
+    random_walk  = fcn_AVAR_generateRandomWalk(random_walk_coefficient,...
+                   sampling_frequency,number_of_time_steps+fir_filter_order); % Random walk
+    random_walk  = random_walk - random_walk(fir_filter_order+1);
+    input_signal = random_walk + white_noise;
+    
+    %% Estimate AVAR of FIR output
+    fir_output_signal = filter(fir_filter_num,1,input_signal);
+    fir_output_signal = fir_output_signal(fir_filter_order+1:end);
+    matrix_fir_avar(:,index_iter) = ...
+        fcn_AVAR_favar([fir_output_signal; 0],list_of_correlation_intervals);
+    
+    if index_iter==number_of_iterations
+        %% Demonstrate FIR filter
+        input_length = 20;
+        input_data   = input_signal(1:fir_filter_order+input_length);
+        fir_output_signal = filter(fir_filter_num,1,input_data);
+        fir_output_signal = fir_output_signal(fir_filter_order+1:end);
+        
+        % Plot the input and its FIR output
+        yLim_min   = min([input_data(fir_filter_order+1:end), fir_output_signal],[],'all');
+        yLim_max   = max([input_data(fir_filter_order+1:end), fir_output_signal],[],'all');
+        yLim_Range = 0.1*(yLim_max-yLim_min);
+        xLim_min   = 0;
+        xLim_max   = input_length-1;
+        xLim_Range = 0.1*(xLim_max-xLim_min);
+        figure(12345)
+        clf
+        width = 540; height = 400; right = 100; bottom = 100;
+        set(gcf, 'position', [right, bottom, width, height])
+        hold on
+        grid on
+        plot((0:input_length-1)',input_data(fir_filter_order+1:end),'k.','Markersize',13)
+        plot((0:input_length-1)',fir_output_signal,'c*','Markersize',8)
+        set(gca,'xtick',[3 7 11 15 19],'Fontsize',13)
+        legend('Input','Output','Location','best','Interpreter','latex','Fontsize',13)
+        ylabel('Amplitude $[Unit]$','Interpreter','latex','Fontsize',18)
+        xlabel('Time Step $[Number \: of \; Samples]$','Interpreter','latex','Fontsize',18)
+        title(['$p =$ ' num2str(fir_filter_order) ', $\omega_{n} =$ ' num2str(normalized_cutoff_frequency)],...
+              'Interpreter','latex','Fontsize',18)
+        ylim([yLim_min-yLim_Range yLim_max+yLim_Range])
+        xlim([xLim_min-xLim_Range xLim_max+xLim_Range])
+        
+        %% Demonstrate AVAR of FIR filter
+        %%% Estimate AVAR of input to MA filter
+        [avar_input,avar_input_lb,avar_input_ub] = ...
+            fcn_AVAR_avarEmpiricalConfidenceCurves([input_signal(1:number_of_time_steps); 0],...
+            list_of_correlation_intervals,confidence_coefficient,noise_type);
+        
+        %%% Estimate AVAR of output of MA filter with confidence bounds
+        avar_output = matrix_fir_avar(:,1);
+        % calculate degrees of freedom of the estimator using empirical equation
+        dof_avar     = 2*(mean(matrix_fir_avar,2).^2)./var(matrix_fir_avar,1,2);
+        chi1_squared = icdf('Chisquare',0.5*(1-confidence_coefficient),dof_avar);
+        chi2_squared = icdf('Chisquare',0.5*(1+confidence_coefficient),dof_avar);
+        % calculate lower and upper confidence surfaces using eq.(31)
+        avar_output_lb = (dof_avar.*avar_output)./chi2_squared;
+        avar_output_ub = (dof_avar.*avar_output)./chi1_squared;
+        
+        %%% Plot AVAR of I/O of FIR filter
+        figure(12346)
+        clf
+        width = 540; height = 448; right = 100; bottom = 100;
+        axis_position = [0.1354, 70.515/height, 0.7696, 307.32/height];
+        set(gcf, 'position', [right, bottom, width, height])
+        hold on
+        grid on
+        plot(list_of_correlation_intervals,avar_input,'k','Linewidth',1.2)
+        plot(list_of_correlation_intervals,avar_input_lb,'k--','Linewidth',1)
+        plot(list_of_correlation_intervals,avar_input_ub,'k-.','Linewidth',1)
+        plot(list_of_correlation_intervals,avar_output,'c','Linewidth',1.2)
+        plot(list_of_correlation_intervals,avar_output_lb,'c--','Linewidth',1)
+        plot(list_of_correlation_intervals,avar_output_ub,'c-.','Linewidth',1)
+        legend_cell = cell(6,1);
+        legend_cell{1} = 'In';
+        legend_cell{2} = ['In: ' num2str(100*confidence_coefficient) '$\%$ LB'];
+        legend_cell{3} = ['In: ' num2str(100*confidence_coefficient) '$\%$ UB'];
+        legend_cell{4} = ['Out: $p =$ ' num2str(fir_filter_order) ', $\omega_{n} =$ ' num2str(normalized_cutoff_frequency)];
+        legend_cell{5} = ['Out: ' num2str(100*confidence_coefficient) '$\%$ LB'];
+        legend_cell{6} = ['Out: ' num2str(100*confidence_coefficient) '$\%$ UB'];
+        legend(legend_cell,'NumColumns',2,'Location','best','Interpreter','latex','FontSize',13)
+        set(gca,'Position',axis_position,'xtick',[1e0 1e2 1e4],'XScale','log',...
+            'YScale','log','FontSize',13)
+        ylabel('Allan Variance $[Unit^2]$','Interpreter','latex','FontSize',18)
+        xlabel('Correlation Interval $[Number \: of \: Samples]$','Interpreter','latex','FontSize',18)
+        ylim([1e-4 1e0])
+        ax1 = gca;
+        axes('Position',axis_position,'XAxisLocation','top',...
+             'xLim',ax1.XLim*sampling_interval,'XScale','log','ytick',[],...
+             'xtick',[1e-1 1e1 1e3],'Color','none','Fontsize',13,'Box','off');
+        ax2 = gca;
+        xlabel(ax2,'Correlation Time $[s]$','Interpreter','latex','FontSize',18)
+
+        %% Calculated Allan Variance of FIR output
+        calculated_avar = fcn_AVAR_avarFIR(power_spectral_density,random_walk_coefficient,...
+            list_of_correlation_intervals,fir_filter_order,fir_filter_num,...
+            sampling_interval,fir_noise_model);
+        
+        %%% Plot calculated AVAR against estimated AVAR
+        figure(12347)
+        clf
+        width = 540; height = 448; right = 100; bottom = 100;
+        axis_position = [0.1354, 70.515/height, 0.7696, 307.32/height];
+        set(gcf, 'position', [right, bottom, width, height])
+        hold on
+        grid on
+        plot(list_of_correlation_intervals,avar_output,'c','Linewidth',1.2)
+        plot(list_of_correlation_intervals,avar_output_lb,'c--','Linewidth',1)
+        plot(list_of_correlation_intervals,avar_output_ub,'c-.','Linewidth',1)
+        plot(list_of_correlation_intervals,calculated_avar,'k','Linewidth',1.2)
+        legend('Estimated',[num2str(100*confidence_coefficient) '$\%$ LB'],...
+               [num2str(100*confidence_coefficient) '$\%$ UB'],'Calculated',...
+               'NumColumns',2,'Location','best','Interpreter','latex','FontSize',13)
+        set(gca,'Position',axis_position,'xtick',[1e0 1e2 1e4],'XScale','log','YScale','log','FontSize',13)
+        ylabel('Allan Variance $[Unit^2]$','Interpreter','latex','FontSize',18)
+        xlabel('Correlation Interval $[Number \: of \: Samples]$','Interpreter','latex','FontSize',18)
+        ylim([1e-4 1e0])
+        ax1 = gca;
+        axes('Position',axis_position,'XAxisLocation','top',...
+             'xLim',ax1.XLim*sampling_interval,'XScale','log','ytick',[],...
+             'xtick',[1e-1 1e1 1e3],'Color','none','Fontsize',13,'Box','off');
+        ax2 = gca;
+        xlabel(ax2,'Correlation Time $[s]$','Interpreter','latex','FontSize',18)
+    end % NOTE: END IF statement 'index_iter==number_of_iterations'
+end % NOTE: END FOR loop 'number_of_iterations'
